1cbc236bb0c0c69d31cba840416230db305fbfb7
bnevins <biz@bnevins.com>
2023-02-28 11:11:34 -0500

    New build stuff with png seems to be working OK. I got rid of concept of having a "dist" dir with the exe's and dll's.  Now they are in Debug or Release folders

--- showart/deploy.bat
+++ showart/deploy.bat
@@ -1,2 +1,8 @@
-
-copy ..\..\dist\*.* "C:\Program Files\Elf\Showart"
+REM copy ..\..\dist\*.* "C:\Program Files\Elf\Showart"
+set target=c:\temp
+echo %target%
+copy e:\dev\cpp\win\showart\Debug\Showartd.exe %target%
+copy e:\dev\cpp\win\showart\Release\Showart.exe %target%
+copy e:\dev\cpp\win\diblib\Debug\bndibd.dll %target%
+copy e:\dev\cpp\win\diblib\Release\bndib.dll %target%
+set target=
