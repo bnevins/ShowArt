eaf61d47c1f245c8256a6dd411d0e61db9a9bb21
Byron Nevins <biz@bnevins.com>
2026-03-07 11:54:43 -0500

    BASE: Verified Local Build in VS 2022 v143 with legacy JPEG libs

--- showart/deploy.bat
+++ showart/deploy.bat
@@ -1,11 +1,12 @@
 REM copy ..\..\dist\*.* "C:\Program Files\Elf\Showart"
-set target=C:\apps\Elf\Showart
+set target=C:\bin\Elf\Showart
 echo %target%
 copy e:\dev\cpp\win\showart\Debug\Showartd.exe %target%
 copy e:\dev\cpp\win\showart\Release\Showart.exe %target%
 copy e:\dev\cpp\win\diblib\Debug\bndibd.dll %target%
 copy e:\dev\cpp\win\diblib\Release\bndib.dll %target%
-set target=C:\google\showart
+set target=%GOOGLE_DRIVE%\showart
+mkdir %target%
 copy e:\dev\cpp\win\showart\Release\Showart.exe %target%
 copy e:\dev\cpp\win\diblib\Release\bndib.dll %target%
 set target=
